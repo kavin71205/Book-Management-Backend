7e2cbd4cd9114545c5c0b77c469250e9df7b490e
Kavin Ponnusamy <kavin712@Kavins-MacBook-Air.local>
2026-01-13 10:57:55 +0530

    Fix port for deployment

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/books", bookRoutes);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
